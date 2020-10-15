7d7cadffcad41168ed1e44f1d47f9290e771c2be
clianor <ejrgus94160@gmail.com>
2020-10-15 22:20:03 +0900

    1.2 Router Setup
    Signed-off-by: clianor <ejrgus94160@gmail.com>

---
+++ src/routes/Auth.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Auth = () => <sapn>Auth</sapn>;
+
+export default Auth;
---
+++ src/routes/Home.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Home = () => <sapn>Home</sapn>;
+
+export default Home;
