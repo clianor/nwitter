e1eb6cf1a7c4c90ca6a706fa7b842ddce8ef0739
clianor <ejrgus94160@gmail.com>
2020-10-17 12:54:07 +0900

    3.2 Getting the Nweets
    Signed-off-by: clianor <ejrgus94160@gmail.com>

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,24 @@
+import React, { useEffect, useState } from "react";
 import { dbService } from "fbase";
-import React, { useState } from "react";
 
 const Home = () => {
   const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+
+  const getNweets = async () => {
+    const dbNweets = await dbService.collection("nweets").get();
+    dbNweets.forEach((document) => {
+      const nweetObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setNweets((prev) => [nweetObject, ...prev]);
+    });
+  };
+
+  useEffect(() => {
+    getNweets();
+  }, []);
 
   const onSubmit = async (event) => {
     event.preventDefault();
@@ -34,6 +50,13 @@ const Home = () => {
         />
         <input type="submit" placeholder="Nweet" />
       </form>
+      <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>
+            <h4>{nweet.nweet}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
