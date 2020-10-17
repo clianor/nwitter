bd0442510a001ff77cf6d597a754db037f5ef083
clianor <ejrgus94160@gmail.com>
2020-10-17 12:19:21 +0900

    3.0 Form and Databse Setup
    Signed-off-by: clianor <ejrgus94160@gmail.com>

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Home = () => <sapn>Home</sapn>;
+const Home = () => {
+  const [nweet, setNweet] = useState("");
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+
+    setNweet(value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={nweet}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+          onChange={onChange}
+        />
+        <input type="submit" placeholder="Nweet" />
+      </form>
+    </div>
+  );
+};
 
 export default Home;
