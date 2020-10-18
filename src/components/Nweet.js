991eba5a4a91ece53ad492620501492560962a5a
clianor <ejrgus94160@gmail.com>
2020-10-18 18:05:35 +0900

    4.3 File URL and Nweet
    Signed-off-by: clianor <ejrgus94160@gmail.com>

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -4,6 +4,7 @@ import { dbService } from "fbase";
 const Nweet = ({ nweetObj, isOwner }) => {
   const [editing, setEditing] = useState(false);
   const [newNweet, setNweNweet] = useState(nweetObj.text);
+
   const onDeleteClick = async () => {
     const ok = window.confirm("Are you sure you want to delete this nweet");
     if (ok) {
@@ -49,6 +50,14 @@ const Nweet = ({ nweetObj, isOwner }) => {
       ) : (
         <>
           <h4>{nweetObj.text}</h4>
+          {nweetObj.attachmentUrl && (
+            <img
+              src={nweetObj.attachmentUrl}
+              alt="nweet_image"
+              width="50px"
+              height="50px"
+            />
+          )}
           {isOwner && (
             <>
               <button onClick={onDeleteClick}>Delete Nweet</button>
