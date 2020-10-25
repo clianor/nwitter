bc042b6a89352832a170cb8646b6a99f80a1a006
clianor <ejrgus94160@gmail.com>
2020-10-25 13:51:46 +0900

    Updates

---
+++ precache-manifest.d651b1b4e895f9b14ca578e97425c6e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61c44966271c27c2975bddd1cfa4897e",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "e7bcbb886abe643016eb",
+    "url": "/nwitter/static/css/main.2ff3d120.chunk.css"
+  },
+  {
+    "revision": "6525727159f28418276a",
+    "url": "/nwitter/static/js/2.a79dd75a.chunk.js"
+  },
+  {
+    "revision": "73fe2f17e0b5f3735cb6289f8d13e5a5",
+    "url": "/nwitter/static/js/2.a79dd75a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7bcbb886abe643016eb",
+    "url": "/nwitter/static/js/main.b0149235.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);
